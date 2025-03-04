0e75d70ef30dfe506b3d17b5e45d5fc97d00a087
yacodev <cyaco33@gmail.com>
2025-03-04 16:58:01 -0500

    modify pokemon page

--- src/pages/Pokemons.tsx
+++ src/pages/Pokemons.tsx
@@ -3,17 +3,29 @@ import { useUserStore } from '../store';
 import { pokemonServices } from '../services/pokemonServices';
 import { Pokemon } from '../interface';
 import { useNavigate } from 'react-router-dom';
-import { CardPokemon, Loading } from '../components';
+import { PokemonCard, Loading, Button } from '../components';
+import { usePokemonCapturedStore } from '../store/pokemonCapturedStore';
 
 export const Pokemons = () => {
   const { name } = useUserStore();
-  //const { listSelectedPokemon } = usePokemonStore();
+  const { listPokemonCaptured, setListPokemonCaptured } =
+    usePokemonCapturedStore();
+
   const navigate = useNavigate();
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-
   const [loading, setLoading] = useState(true);
 
-  //const { setSelectedPokemon } = usePokemonStore();
+  useEffect(() => {
+    const getAllPokemons = async () => {
+      setLoading(true);
+
+      await getListPokemons();
+
+      setLoading(false);
+    };
+
+    getAllPokemons();
+  }, []);
 
   const getListPokemons = async () => {
     try {
@@ -41,29 +53,25 @@ export const Pokemons = () => {
     }
   };
 
-  useEffect(() => {
-    const getAllPokemons = async () => {
-      setLoading(true);
-
-      await getListPokemons();
-
-      setLoading(false);
-    };
-
-    getAllPokemons();
-  }, []);
-
-  const handleNavigate = (pokemon: Pokemon) => {
-    //setSelectedPokemon(pokemon);
-    console.log(pokemon);
-    navigate('/details');
+  const handleNavigateToDetails = (id: number) => {
+    navigate('/details/' + id);
   };
+
+  const handleNavigateToCaptured = () => {
+    navigate('/captured');
+  };
+
+  const handlePokemonCapture = (pokemon: Pokemon) => {
+    console.log('pokemon captured', pokemon);
+    setListPokemonCaptured(pokemon);
+    console.log('listPokemonCaptured', listPokemonCaptured);
+  };
+
+  const userLogged = name !== '';
 
   if (loading) {
     return <Loading />;
   }
-
-  const showLoginMessage = name !== '';
 
   return (
     <div className='max-w-7xl mx-auto flex flex-col items-center justify-center'>
@@ -72,28 +80,26 @@ export const Pokemons = () => {
           <h1 className='text-2xl font-bold text-gray-900 '>
             Bienvenido, {name}!
           </h1>
-          {showLoginMessage ? (
+          {userLogged ? (
             <span className='text-blue-500  text-2xl'>Logueado</span>
           ) : (
             <span className='text-red-500 text-2xl '>No logueado</span>
           )}
         </div>
-        <button
-          onClick={() => navigate('/history')}
-          className='px-4 py-2 bg-blue-500 hover:bg-blue-600 
-                         text-white  rounded-lg
-                         flex items-center '
-        >
-          Ver pokemones capturados
-        </button>
+        <Button
+          text='ver pokemones capturas'
+          onClick={handleNavigateToCaptured}
+        />
       </div>
 
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 w-full px-4'>
         {pokemons.map((pokemon) => (
-          <CardPokemon
+          <PokemonCard
             pokemon={pokemon}
-            handleNavigate={handleNavigate}
+            handleNavigate={handleNavigateToDetails}
             key={pokemon.name}
+            showCaptureButton={userLogged}
+            handleCapture={handlePokemonCapture}
           />
         ))}
       </div>
