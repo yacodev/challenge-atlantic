6632205a589c2ace5e8d06653c91d9fb1c4e6e7b
yacodev <cyaco33@gmail.com>
2025-03-04 16:57:17 -0500

    refactor Loading  component

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,9 +1,7 @@
 export const Loading = () => {
   return (
-    <div className='min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900'>
-      <p className='text-xl text-gray-800 dark:text-white'>
-        Cargando Pokémons...
-      </p>
+    <div className='min-h-screen flex items-center justify-center bg-gray-100 '>
+      <p className='text-xl text-gray-800 '>Cargando Pokémons...</p>
     </div>
   );
 };
