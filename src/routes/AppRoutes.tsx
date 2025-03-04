8cbe0eead790ec3e6b8c5af8995798cc358a1eeb
yacodev <cyaco33@gmail.com>
2025-03-04 16:58:10 -0500

    modify routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import { Login } from '../pages/Login';
 import { Pokemons } from '../pages/Pokemons';
-import { Details } from '../pages/Details';
+import { PokemonDetails } from '../pages/Details';
 import { PokemonsCaptured } from '../pages/PokemonsCaptured';
 
 const AppRoutes = () => {
@@ -9,7 +9,7 @@ const AppRoutes = () => {
     <Routes>
       <Route path='/' element={<Login />} />
       <Route path='/pokemons' element={<Pokemons />} />
-      <Route path='/details' element={<Details />} />
+      <Route path='/details/:id' element={<PokemonDetails />} />
       <Route path='/captured' element={<PokemonsCaptured />} />
     </Routes>
   );
--- src/store/interfaces/index.ts
+++ src/store/interfaces/index.ts
@@ -1,1 +1,2 @@
 export * from './userStore.interface';
+export * from './pokemonCaptureState.interface';
