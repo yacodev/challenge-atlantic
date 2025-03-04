d59493d0b407a7ac1698b20b9b4a460eeaa45b24
yacodev <cyaco33@gmail.com>
2025-03-04 15:12:21 -0500

    Add Loading component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,9 @@
+export const Loading = () => {
+  return (
+    <div className='min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900'>
+      <p className='text-xl text-gray-800 dark:text-white'>
+        Cargando Pokémons...
+      </p>
+    </div>
+  );
+};
